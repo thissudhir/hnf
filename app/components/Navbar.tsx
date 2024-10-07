966928b443e5732026026ba37f10a129d8906785
Abhishek Yadav <abhishekpsitfor4years@gmail.com>
2024-10-07 20:57:06 +0530

    added buttons to navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,17 +1,21 @@
-import { AppBar, Button, Toolbar, Typography } from "@mui/material";
+import { AppBar, Box, Button, Toolbar, Typography } from "@mui/material";
 import Link from "next/link";
 
 const Navbar = () => {
   return (
     <AppBar elevation={0} style={{ backgroundColor: "#F0EFE1" }}>
-      <Toolbar>
-        <Typography
-          variant="h6"
-          component="div"
-          sx={{ flexGrow: 1, color: "#000" }}
-        >
+      <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
+        <Typography variant="h6" component="div" sx={{ color: "#000" }}>
           Humanity Network Foundation
         </Typography>
+        <Box sx={{ display: "flex", gap: "10px", color: "#000" }}>
+          <Box>Home</Box>
+          <Box>Welcome</Box>
+          <Box>Event Details</Box>
+          <Box>About</Box>
+          <Box>Projects</Box>
+          <Box>Instagram</Box>
+        </Box>
         <Button variant="outlined">
           <Link href="/" style={{ textDecoration: "none", color: "#000" }}>
             Donate
